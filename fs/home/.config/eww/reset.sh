2719fafbe39636b15e89990e793aa085e868aa94
Vidar Magnusson <git@vidarmagnusson.com>
2025-08-29 11:22:14 +0200

    Work on eww

--- fs/home/.config/eww/reset.sh
+++ fs/home/.config/eww/reset.sh
@@ -2,5 +2,6 @@
 
 killall eww
 eww daemon --debug
-eww open-many bar_center_0 bar_center_1 bar_left_0 bar_left_1 bar_right_0 bar_right_1
+eww open-many bar_center_0 bar_center_1 
+eww open-many bar_left_0 bar_left_1 bar_right_0 bar_right_1
 
